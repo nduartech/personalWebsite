aa91a3abec00a2ea8abe22716919589cef9ff1e2
nduartech <nduartech101@gmail.com>
2024-05-24 06:42:37 -0400

    Transitions

--- packages/software-eng/src/spa.tsx
+++ packages/software-eng/src/spa.tsx
@@ -1,14 +1,18 @@
 import App from "./App";
 import { render } from 'solid-js/web';
 import singleSpaSolid from '@nathanld/single-spa-solid';
+import { cssLifecycleFactory } from 'vite-plugin-single-spa/ex';
 
 const solidLifecycles = singleSpaSolid({
     solid: {render: render},
     rootComponent: App,
     domElementGetter: () => document.getElementById('app2')
 });
+
+const cssLc = cssLifecycleFactory('spa',);
+
 let domEl: HTMLDivElement;
-export const bootstrap = (props:any)=> {
+export const bootstrap = [cssLc.bootstrap, (props:any)=> {
     return Promise.resolve().then(()=>{
         domEl = document.createElement('div');
         domEl.id = 'app2';
@@ -17,16 +21,17 @@ export const bootstrap = (props:any)=> {
         root && root.appendChild(domEl);
         solidLifecycles.bootstrap().then();
     });
-}
-export const mount = (props:any) => {
+}];
+export const mount = [cssLc.mount, (props:any) => {
     return Promise.resolve().then(()=>{
         window.requestAnimationFrame(() => domEl.classList.add('bounceInUp'));
         solidLifecycles.mount(props).then();
     });
-};
-export const unmount = (props:any) => {
+}];
+
+export const unmount = [cssLc.unmount, (props:any) => {
     return Promise.resolve().then(()=>{
         domEl.classList.remove('animated', 'bounceInUp');
         solidLifecycles.unmount(props).then();
     })
-};
+}];
