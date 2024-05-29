fde18d05bd051ea418d793b9344424542f0bb709
nduartech <nduartech101@gmail.com>
2024-05-28 21:40:56 -0400

    Progress

--- packages/software-eng/src/components/skill/SkillBar.tsx
+++ packages/software-eng/src/components/skill/SkillBar.tsx
@@ -7,7 +7,7 @@ function SkillBar(props: any) {
         backdrop-filter backdrop-blur-[2px] rounded-[10px] border-[1px] border-[solid] border-[rgba(255,255,255,0.18)]
         dark:bg-[rgba(_0,_0,_0,_0.7_)] dark:[box-shadow:0_8px_32px_0_rgba(_31,_38,_135,_0.37_)] dark:backdrop-filter
         dark:backdrop-blur-[14.5px] dark:rounded-[10px] dark:border-[1px] dark:border-[solid] dark:border-[rgba(255,255,255,0.18)]
-        hidden md:hidden lg:flex flex-row justify-start items-center px-0 md:px-5 lg:px-10">
+        sm:hidden md:hidden lg:flex xl:flex 2xl:flex flex-row justify-start items-center px-0 md:px-5 lg:px-10">
       <div class="flex flex-row flex-wrap w-12/12 h-11/12  py-2 justify-center items-center"
         // no-scrollbar scroll-smooth
         // " onWheel={(event)=> {
