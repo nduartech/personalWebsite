1793a8a03f8a7ea311c6b8cb199150946ddda8f2
nduartech <nduartech101@gmail.com>
2024-08-08 22:59:19 -0400

    Revert "Tweaks"
    This reverts commit 3a16a2a85f605031ae034c27329ae7a10fb30f49.

--- packages/host/astro.config.ts
+++ packages/host/astro.config.ts
@@ -1,16 +1,14 @@
-import qwik from '@qwikdev/astro';
-import { defineConfig } from 'astro/config';
+import qwik from '@qwikdev/astro'
+import { defineConfig } from 'astro/config'
 // import node from "@astrojs/node";
-import tailwind from '@astrojs/tailwind';
-import svelte from '@astrojs/svelte';
-import solidJs from '@astrojs/solid-js';
-import compressor from 'astro-compressor';
-import sitemap from '@astrojs/sitemap';
-import robotsTxt from 'astro-robots-txt';
-import netlify from '@astrojs/netlify';
-import mdx from '@astrojs/mdx';
-
-import sentry from "@sentry/astro";
+import tailwind from '@astrojs/tailwind'
+import svelte from '@astrojs/svelte'
+import solidJs from '@astrojs/solid-js'
+import compressor from 'astro-compressor'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
+import netlify from '@astrojs/netlify'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,47 +16,51 @@ export default defineConfig({
   output: "hybrid",
   adapter: netlify(),
   experimental: {
-    serverIslands: true
+    serverIslands: true,
   },
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
   site: 'https://nathanld.xyz',
   prefetch: {
-    prefetchAll: true
+    prefetchAll: true,
   },
   // output: "server",
   // adapter: node({
   //   mode: "standalone",
   // }),
-  integrations: [sentry({
-    dsn: "https://b3e71133ff7ca12d03de6f854827b87a@o4507745069301760.ingest.us.sentry.io/4507745077559296",
-    sourceMapsUploadOptions: {
-      project: 'nathanld-xyz',
-      authToken: process.env.SENTRY_AUTH_TOKEN
-    }
-  }),
+  integrations: [
     qwik({
-    include: '**/qwik/*'
-  }), tailwind({
-    applyBaseStyles: false
-  }), svelte({
-    include: '**/svelte/*'
-  }), solidJs({
-    include: '**/solid/*'
-  }), {
-    name: 'importmap-externals',
-    hooks: {
-      'astro:build:setup': ({
-        vite,
-        target
-      }) => {
-        if (target === 'client') {
-          if (vite.build?.rollupOptions) {
-            vite.build.rollupOptions.external = ['software-eng', 'interests', 'solid-js'];
+      include: '**/qwik/*',
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    svelte({
+      include: '**/svelte/*',
+    }),
+    solidJs({
+      include: '**/solid/*',
+    }),
+    {
+      name: 'importmap-externals',
+      hooks: {
+        'astro:build:setup': ({ vite, target }) => {
+          if (target === 'client') {
+            if (vite.build?.rollupOptions) {
+              vite.build.rollupOptions.external = [
+                'software-eng',
+                'interests',
+                'solid-js',
+              ]
+            }
           }
-        }
-      }
-    }
-  }, mdx(), sitemap(), robotsTxt(), compressor()]
-});
+        },
+      },
+    },
+    mdx(),
+    sitemap(),
+    robotsTxt(),
+    compressor(),
+  ],
+})
