e5f7f55a708f6ee432b52c7eae4449799d734b5e
nduartech <nduartech101@gmail.com>
2024-12-15 04:48:01 -0500

    blog nav

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,13 +1,49 @@
 <script lang="ts">
-import me from '$lib/assets/me.png?url'
-import ModeToggle from '$lib/components/darkMode.svelte'
-import * as Avatar from '$lib/components/ui/avatar/index.js'
+    import me from '$lib/assets/me.png?url'
+    import ModeToggle from '$lib/components/darkMode.svelte'
+    import * as Avatar from '$lib/components/ui/avatar/index.js'
+    import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js"
+
+    let {pathname} = $props();
+    let pathElements = pathname.split('/').slice(1, -1);
+    pathElements = pathElements.map((pathElement, j)=>{
+        let path = "";
+        for(let i = 0; i <= j; i++){
+            path += "/"+pathElements[i];
+        }
+        if (pathElement === "blog") return ["Technical Blog",path];
+        return [pathElement,path];
+    });
+    let lastElement = pathname.split('/').pop();
 </script>
 
 <div class="flex flex-row w-full justify-between p-10">
-    <Avatar.Root>
-      <Avatar.Image src={me} alt="@me" />
-      <Avatar.Fallback>ND</Avatar.Fallback>
-    </Avatar.Root>
-    <ModeToggle />
+    <div class="flex flex-row w-full justify-start space-x-10">
+        <Avatar.Root>
+            <Avatar.Image src={me} alt="@me"/>
+            <Avatar.Fallback>ND</Avatar.Fallback>
+        </Avatar.Root>
+        {#if pathname !== '/'}
+            <Breadcrumb.Root>
+                <Breadcrumb.List>
+                    <Breadcrumb.Item>
+                        <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+                    </Breadcrumb.Item>
+                    {#if pathElements.length > 0}
+                        {#each pathElements as pathElement}
+                            <Breadcrumb.Separator/>
+                            <Breadcrumb.Item>
+                                <Breadcrumb.Link href={pathElement[1]}>{pathElement[0]}</Breadcrumb.Link>
+                            </Breadcrumb.Item>
+                        {/each}
+                    {/if}
+                    <Breadcrumb.Separator/>
+                    <Breadcrumb.Item>
+                        <Breadcrumb.Page>{document.title}</Breadcrumb.Page>
+                    </Breadcrumb.Item>
+                </Breadcrumb.List>
+            </Breadcrumb.Root>
+        {/if}
+    </div>
+    <ModeToggle/>
 </div>
---
+++ src/components/ui/breadcrumb/breadcrumb-separator.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import ChevronRight from '~icons/line-md/chevron-right?raw';
+	import type { WithElementRef } from "bits-ui";
+	import type { HTMLLiAttributes } from "svelte/elements";
+	import { cn } from "$lib/utils.js";
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	}: WithElementRef<HTMLLiAttributes> = $props();
+</script>
+
+<li
+	role="presentation"
+	aria-hidden="true"
+	class={cn("[&>svg]:size-3.5", className)}
+	bind:this={ref}
+	{...restProps}
+>
+	{#if children}
+		{@render children?.()}
+	{:else}
+		<span>{@html ChevronRight}</span>
+	{/if}
+</li>
