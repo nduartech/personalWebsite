4648b55b5754f17ebac6f98b2a7d220d45ec4010
nduartech <nduartech101@gmail.com>
2024-12-15 03:25:11 -0500

    blog

---
+++ src/components/blog/allPosts.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import * as Card from "$lib/components/ui/card/index.js";
+    import {onMount} from "svelte";
+    import {Badge} from "$lib/components/ui/badge/index.js";
+    import {Label} from "$lib/components/ui/label/index.js";
+
+    let {posts} = $props();
+    let sortedPosts = $state(posts.sort((a, b) => {
+        return new Date(b.data.published) - new Date(a.data.published);
+    }));
+    let sortPublished = $state(false); // false = newest first, true = oldest first
+</script>
+
+<main class="w-10/12 container pt-10 pb-20">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    {#each sortedPosts as post}
+        <div class="block">
+        <a class="block" href={"/blog/"+post.data.slug}>
+        <Card.Root>
+            <Card.Header>
+                <Card.Title>{post.data.title}</Card.Title>
+                <Card.Description>{post.data.description} | Published on: {post.data.published.toLocaleDateString('en-GB', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit'
+                })}</Card.Description>
+            </Card.Header>
+            <Card.Content class="py-2 flex flex-row items-start w-full overflow-x-wrap">
+                <Label for={post.data.id} class="pt-1 pr-2 text-xs">tags:</Label>
+                <div id={post.data.id} class="flex flex-row flex-wrap space-x-1">
+                    {#each post.data.tags as tag}
+                        <span class="mb-0.5"><Badge variant="primary">{tag}</Badge></span>
+                    {/each}
+                </div>
+            </Card.Content>
+            <Card.Footer>
+                {#if (post.data.series)}
+                    <Label for={post.data.series.id} class="pr-2 text-xs">series:</Label>
+                    <Badge id={post.data.series.id} variant="secondary">{post.data.series.id}</Badge>
+                {/if}
+            </Card.Footer>
+        </Card.Root>
+        </a>
+        </div>
+    {/each}
+    </div>
+</main>
--- src/components/landing/landing.svelte
+++ src/components/landing/landing.svelte
@@ -1,20 +1,15 @@
 <script lang="ts">
-import Header from '$lib/components/header.svelte'
 import Command from '$lib/components/landing/landingCommand.svelte'
 let {articles} = $props();
 </script>
-
-<div id="landing" class="h-full w-full bg-white dark:bg-black">
-    <Header />
-    <main>
-        <div class="w-full h-full flex justify-center items-center">
-          <div class="flex flex-col w-full place-items-center space-y-8">
+<main>
+    <div class="w-full h-full flex justify-center items-center">
+        <div class="flex flex-col w-full place-items-center space-y-8">
             <h1 class="lg:text-6xl text-black dark:text-white">Hello, I'm Nathan</h1>
             <h3>Software Engineer</h3>
             <div class="flex w-full px-20">
-                <Command articles={articles} />
+                <Command articles={articles}/>
             </div>
-          </div>
         </div>
-    </main>
-</div>
+    </div>
+</main>
