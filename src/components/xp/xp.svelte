30bf99fc77f4f61f8bd850695a2ccf505bcc12ba
nduartech <nduartech101@gmail.com>
2024-12-16 09:48:20 -0500

    finishing touches

--- src/components/xp/xp.svelte
+++ src/components/xp/xp.svelte
@@ -116,7 +116,7 @@
                                                 </div>
                                             </a>
                                             <div class="flex flex-row justify-center items-center cursor-pointer"
-                                                 on:click={() => fetch('https://raw.githubusercontent.com/nduartech/personalWebsite/refs/heads/back2basics/src/assets/resume.pdf').then(resp => resp.blob()).then(blob => {
+                                                 on:click={() => fetch('https://raw.githubusercontent.com/nduartech/personalWebsite/refs/heads/pro/src/assets/resume.pdf').then(resp => resp.blob()).then(blob => {
                                                      const url = URL.createObjectURL(blob);
                                                      const a = document.createElement("a");
                                                      a.style.display = "none";
