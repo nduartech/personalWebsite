49a856b5cfe84477ed5c540c0f39fe519160b0f8
nduartech <nduartech101@gmail.com>
2024-12-16 23:28:10 -0500

    finishing touches

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import deno from '@deno/astro-adapter'
 import { defineConfig, envField } from 'astro/config'
 import Icons from 'unplugin-icons/vite'
 
-import expressiveCode from 'astro-expressive-code'
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nathanld.xyz',
