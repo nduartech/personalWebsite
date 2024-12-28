c13c096bf1f4a3febeba014bab915d5c2647b9c7
nduartech <nduartech101@gmail.com>
2024-12-27 20:58:22 -0500

    Github Pages

--- src/components/landing/landingCommand.svelte
+++ src/components/landing/landingCommand.svelte
@@ -98,9 +98,9 @@ function navigateToPage(id: string|undefined) {
             }
         }} onmouseenter={()=>{openCommandMenu()}}
              onmousedown={()=>{openCommandMenu()}}
-             onclick={()=>{openCommandMenu()}} role="combobox" tabindex="0" aria-controls="command-root" aria-autocomplete="list" aria-haspopup="menu" aria-level="3" aria-multiline="false" aria-multiselectable="false" aria-orientation="horizontal" aria-readonly="false" aria-required="false" aria-sort="ascending" aria-pressed={open?"true":"false"} aria-selected={open?"true":"false"} aria-expanded={open?"true":"false"}
+             onclick={()=>{openCommandMenu()}} role="combobox" tabindex="0" aria-controls="command-root" aria-autocomplete="list" aria-haspopup="menu" aria-readonly="false" aria-required="false" aria-expanded={open?"true":"false"}
              aria-label="Search work, projects, and articles">
-            <Command.Input bind:value={searchContent} bind:ref={searchBox} bind:open={open} aria-label="Search work, projects, and articles" onClose={closeCommandMenu} class="w-full" placeholder=""/>
+            <Command.Input bind:value={searchContent} bind:ref={searchBox} bind:open={open} bind:aria-label="Search work, projects, and articles" onClose={closeCommandMenu} class="w-full" placeholder=""/>
         </div>
         <Command.List class={open?"no-scrollbar":"hidden"} onclick={useSelectedValue}>
             <Command.Empty>No results found</Command.Empty>
